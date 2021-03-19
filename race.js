2d114612def3966197198146131acff2a47183db
koleaabramovici <78415812+koleaabramovici@users.noreply.github.com>
2021-03-19 17:06:38 +0200

    My 4th exercise in JS

---
+++ race.js
@@ -0,0 +1,24 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly = true;
+
+let runnersAge = 23;
+
+if(runnersAge > 18 && registeredEarly){
+  raceNumber += 1000;
+  } 
+
+if(runnersAge>18 && registeredEarly){
+  console.log(`${raceNumber} : Your race starts at 9:30 am.`);
+  }  else if(runnersAge>18 && !registeredEarly){
+  console.log(`${raceNumber} : Your race starts at 11:00 am.`);
+  }  else if(runnersAge<18){
+    console.log(`${raceNumber} : Your race starts at 12:30 pm.`);
+  }
+
+
+
+
+
+
+
